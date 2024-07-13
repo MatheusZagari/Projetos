a29ede48ee131eeb9490ce2d877e7149cff223aa
MatheusZagari <137003161+MatheusZagari@users.noreply.github.com>
2024-07-13 20:16:44 -0300

    Update NovoProduto.java

--- BancoDeDados/src/main/java/teste/basico/NovoProduto.java
+++ BancoDeDados/src/main/java/teste/basico/NovoProduto.java
@@ -5,14 +5,16 @@ import infra.DAO;
 
 public class NovoProduto {
     public static void main(String[] args) {
-        Produto produto = new Produto("Caneta", 7.45);
+        Produto produto = new Produto("Monitor 23",  789.89);
 
         DAO<Produto> dao = new DAO<>(Produto.class);
         try {
-            dao.abrirTransacao().incluirAtomico(produto).fecharTransacao().fechar();
+            dao.incluirAtomico(produto).fechar();
             System.out.println("Produto adicionado com sucesso!");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println("Id do produto: " + produto.getId());
     }
 }
