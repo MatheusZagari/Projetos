cbb4b08f5adea379a09d07b2bb339145df05f801
MatheusZagari <137003161+MatheusZagari@users.noreply.github.com>
2024-07-22 23:34:44 -0300

    JavaFX

---
+++ BancoDeDados/src/main/java/app-financeiro/module-info.java
@@ -0,0 +1,8 @@
+module app.financeiro {
+    requires java.desktop;
+    requires java.persistence;
+    requires java.sql;
+    requires mysql.connector.java;
+    requires org.hibernate.orm.core;
+
+}
---
+++ app-calculo/src/main/java/br/com/cod3r/app/calculo/Calculadora.java
@@ -0,0 +1,6 @@
+package br.com.cod3r.app.calculo;
+
+public class Calculadora {
+    private operacoesArithmeticas operacoesArithmeticas;
+
+}
